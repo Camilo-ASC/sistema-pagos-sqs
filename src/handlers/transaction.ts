0ff74ff815251e368cf70a73210139e493f63b26
Camilo-ASC <Camiloasc30@gmail.com>
2026-04-23 22:53:08 -0500

    mapping table name

--- src/handlers/transaction.ts
+++ src/handlers/transaction.ts
@@ -6,14 +6,23 @@ const dynamo = new DynamoDB.DocumentClient();
 export const handler = async (event: any) => {
     for (const record of event.Records) {
         const { traceId, cardId, service } = JSON.parse(record.body);
+        
         // Retraso obligatorio de 5 segundos
         await new Promise(r => setTimeout(r, 5000));
-        // Persiste el pago final en DynamoDB
+
+        // Persiste el pago final en DynamoDB usando el nombre del proyecto
         await dynamo.put({
-            TableName: 'payment',
-            Item: { traceId, cardId, service, status: 'FINISH', timestamp: Date.now() }
+            TableName: 'sistema-pagos-logicapagos-payments', // <--- CAMBIADO AQUÍ
+            Item: { 
+                traceId, 
+                cardId, 
+                service, 
+                status: 'FINISH', 
+                timestamp: Date.now() 
+            }
         }).promise();
+
         // Marca la transacción como completada en Redis para el polling
-        await updateStatus(traceId, 'FINISH');
+        await updateStatus(traceId, 'FINISH', { cardId, service });
     }
 };
